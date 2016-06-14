5e2abf04dac4e931c6a492e4d0e0f6004b6ea01a
chenpeng <1632179320@qq.com>
2016-06-14 10:58:09 +0800

    learn async#compose

--- study-api/asyncUse.js
+++ study-api/asyncUse.js
@@ -1,108 +1,108 @@
 var async = require('async');
 var utils = require('./util/asyncHelper');
 
-// //并行运行任务，最后将所有的结果一起回调
-// async.parallel([
-//     async.apply(utils.inc, 3),
-//     async.apply(utils.fire, 10)
-// ], function(err, data) {
-//    utils.log('[test apply] err: ', err);
-//    utils.log('[test apply] data: ', data); 
-// });
+//并行运行任务，最后将所有的结果一起回调
+async.parallel([
+    async.apply(utils.inc, 3),
+    async.apply(utils.fire, 10)
+], function(err, data) {
+   utils.log('[test apply] err: ', err);
+   utils.log('[test apply] data: ', data); 
+});
 
-// function add (a, b, callback, timeout) {
-//     timeout = timeout || 200;
-//     utils.wait(timeout);
-//     callback(null, a+b);
-// }
-// var fn = async.apply(add, 1, 2);
-// fn(function(err, data) {
-//     utils.log('[test apply2] err: ', err); 
-//     utils.log('[test apply2] data: ', data); 
-// });
+function add (a, b, callback, timeout) {
+    timeout = timeout || 200;
+    utils.wait(timeout);
+    callback(null, a+b);
+}
+var fn = async.apply(add, 1, 2);
+fn(function(err, data) {
+    utils.log('[test apply2] err: ', err); 
+    utils.log('[test apply2] data: ', data); 
+});
 
-// /**
-//  * 并行，可以实现给一数组中每个函数传相同参数，通过callback返回
-//  */
-// async.applyEach([
-//     function(a, callback) {
-//         setTimeout(function() {
-//             utils.log('[test applyEach_infn] data: ', a*2);
-//             callback(null, a*2);
-//         }, 500);
-//     }, function(a, callback) {
-//         setTimeout(function() {
-//             utils.log('[test applyEach_infn] data: ', a);
-//             callback(null, a);
-//         }, 500);
-//     }
-// ], 10, function(err, data) {
-//     utils.log('[test applyEach] err: ', err); 
-//     utils.log('[test applyEach] data: ', data); 
-// });
+/**
+ * 并行，可以实现给一数组中每个函数传相同参数，通过callback返回
+ */
+async.applyEach([
+    function(a, callback) {
+        setTimeout(function() {
+            utils.log('[test applyEach_infn] data: ', a*2);
+            callback(null, a*2);
+        }, 500);
+    }, function(a, callback) {
+        setTimeout(function() {
+            utils.log('[test applyEach_infn] data: ', a);
+            callback(null, a);
+        }, 500);
+    }
+], 10, function(err, data) {
+    utils.log('[test applyEach] err: ', err); 
+    utils.log('[test applyEach] data: ', data); 
+});
 
-// /**
-//  * 串行，applyEachSeries与applyEach唯一不同的是，数组的函数同步执行
-//  */
-// async.applyEachSeries([
-//     function(a, callback) {
-//         setTimeout(function() {
-//             utils.log('[test applyEachSeries_infn] data: ', a*2); 
-//             callback(null, a*2);
-//         }, 500);
-//     }, function(a, callback) {
-//         setTimeout(function() {
-//             utils.log('[test applyEachSeries_infn] data: ', a);
-//             callback(null, a);
-//         }, 500);
-//     }
-// ], 20, function(err, data) {
-//     utils.log('[test applyEachSeries] err: ', err); 
-//     utils.log('[test applyEachSeries] data: ', data); 
-// });
+/**
+ * 串行，applyEachSeries与applyEach唯一不同的是，数组的函数同步执行
+ */
+async.applyEachSeries([
+    function(a, callback) {
+        setTimeout(function() {
+            utils.log('[test applyEachSeries_infn] data: ', a*2); 
+            callback(null, a*2);
+        }, 500);
+    }, function(a, callback) {
+        setTimeout(function() {
+            utils.log('[test applyEachSeries_infn] data: ', a);
+            callback(null, a);
+        }, 500);
+    }
+], 20, function(err, data) {
+    utils.log('[test applyEachSeries] err: ', err); 
+    utils.log('[test applyEachSeries] data: ', data); 
+});
 
-// /**
-//  * auto用来处理有依赖关系的多个任务的执行
-//  */
-// /**
-//  * 我要写一个程序，它要完成以下几件事：
-//  * 1. 从某处取得数据
-//  * 2. 在硬盘上建立一个新的目录
-//  * 3. 将数据写入到目录下某文件
-//  * 4. 发送邮件，将文件以附件形式发送给其它人。
-//  *
-//  * 分析该任务，可以知道1与2可以并行执行，3需要等1和2完成，4要等3完成。
-//  * 可以按以下方式来使用auto函数。
-//  */
-// async.auto({
-//     getData: function(callback) {
-//         setTimeout(function() {
-//             console.log('got data');
-//             callback(null, 'mydata');
-//         }, 200);
-//     },
-//     makeFolder: function(callback) {
-//         setTimeout(function() {
-//             console.log('made folder');
-//             callback(null, 'myFolder'); 
-//         }, 200);
-//     },
-//     writeFile: ['getData', 'makeFolder', function(result, callback) {
-//         setTimeout(function() {
-//             console.log('write file');
-//             callback(null, 'myfile'); 
-//         }, 200);
-//     }],
-//     emailFiles: ['writeFile', function(result, callback) {
-//         setTimeout(function() {
-//             console.log(result.writeFile);
-//             callback(null, result.writeFile)
-//         }, 200);
-//     }]
-// }, function(err, data) {
-//     utils.log('[test auto] err: ', err); 
-//     utils.log('[test auto] data: ', data); 
-// });
+/**
+ * auto用来处理有依赖关系的多个任务的执行
+ */
+/**
+ * 我要写一个程序，它要完成以下几件事：
+ * 1. 从某处取得数据
+ * 2. 在硬盘上建立一个新的目录
+ * 3. 将数据写入到目录下某文件
+ * 4. 发送邮件，将文件以附件形式发送给其它人。
+ *
+ * 分析该任务，可以知道1与2可以并行执行，3需要等1和2完成，4要等3完成。
+ * 可以按以下方式来使用auto函数。
+ */
+async.auto({
+    getData: function(callback) {
+        setTimeout(function() {
+            console.log('got data');
+            callback(null, 'mydata');
+        }, 200);
+    },
+    makeFolder: function(callback) {
+        setTimeout(function() {
+            console.log('made folder');
+            callback(null, 'myFolder'); 
+        }, 200);
+    },
+    writeFile: ['getData', 'makeFolder', function(result, callback) {
+        setTimeout(function() {
+            console.log('write file');
+            callback(null, 'myfile'); 
+        }, 200);
+    }],
+    emailFiles: ['writeFile', function(result, callback) {
+        setTimeout(function() {
+            console.log(result.writeFile);
+            callback(null, result.writeFile)
+        }, 200);
+    }]
+}, function(err, data) {
+    utils.log('[test auto] err: ', err); 
+    utils.log('[test auto] data: ', data); 
+});
 
 /**
  * cargo也是一个串行的消息队列，类似于queue，通过限制了worker数量，不再一次性全部执行。
@@ -159,4 +159,34 @@ cargo.push({name: 'C'}, function(err) {
 cargo.push({name: 'D'}, function(err) {
    utils.wait(200);
    utils.log('', 'finished processe D'); 
+});
+
+/**
+ * compose 组织一系列异步的方法的执行，每个方法需要消费上一个方法的返回值
+ * 把f(),g(),h()异步函数，组合成f(g(h()))的形式，通过callback得到返回值
+ */
+//首先定义几个方法
+function compose_A(n, callback) {
+    utils.log('compose_A enter: ', n);
+    setTimeout(function() {
+        callback(null, n + 'A');
+    }, 100);
+}
+function compose_B(n, callback) {
+    utils.log('compose_B enter: ', n);
+    setTimeout(function() {
+        callback(null, n + 'B');
+    }, 100);
+}
+function compose_C(n, callback) {
+    utils.log('compose_C enter: ', n);
+    setTimeout(function() {
+        callback(null, n + 'C');
+    }, 100);
+}
+//创建 compose 实例
+var abc = async.compose(compose_C, compose_B, compose_A);
+abc('start', function(err, result) {
+    utils.log('compose err: ', err);
+    utils.log('compose result: ', result);
 });
